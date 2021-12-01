236e9f96af395dd49afd148d2c8021ed915ded8b
Sayit <sayit358@gmail.com>
2021-12-01 02:29:34 -0600

    Fix tapping term

--- keyboards/crkbd/keymaps/sayit/config.h
+++ keyboards/crkbd/keymaps/sayit/config.h
@@ -31,7 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL_PD2
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+#define TAPPING_TERM 200
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
     #define RGBLIGHT_ANIMATIONS
@@ -43,7 +43,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
-
-
+#define AUTO_SHIFT_TIMEOUT 150
 
 #define SPLIT_WPM_ENABLE
